371f5b21df28baf0fb09125e15da1680a2a9ce25
mallalokesh <lokeshmalla09@gmail.com>
2016-09-17 14:59:13 +0530

    testing

---
+++ SmsGateWay/src/Java_example_httprequest.java
@@ -0,0 +1,38 @@
+import java.net.*;
+
+public class Java_example_httprequest {
+
+        public static void main(String[] args) {
+                try {
+                        String recipient = "9840514538";
+                        String message = "Hello World";
+                        String username = "admin";
+                        String password = "@Ozeki123@";
+                        String originator = "9985788526";
+
+                        String requestUrl  = "http://127.0.0.1:9501/api?action=sendmessage&" +
+            "username=" + URLEncoder.encode(username, "UTF-8") +
+            "&password=" + URLEncoder.encode(password, "UTF-8") +
+            "&recipient=" + URLEncoder.encode(recipient, "UTF-8") +
+            "&messagetype=SMS:TEXT" +
+            "&messagedata=" + URLEncoder.encode(message, "UTF-8") +
+            "&originator=" + URLEncoder.encode(originator, "UTF-8") +
+            "&serviceprovider=SMPP4" +
+            "&responseformat=html";
+
+
+
+                        URL url = new URL(requestUrl);
+                        HttpURLConnection uc = (HttpURLConnection)url.openConnection();
+
+                        System.out.println(uc.getResponseMessage());
+
+                        uc.disconnect();
+
+                } catch(Exception ex) {
+                        System.out.println(ex.getMessage());
+
+                }
+        }
+
+}
